86b7f9fae49e9dce717e74b18a01c9f0690d87e2
Thiago alves <gt584138@gmail.com>
2025-08-31 15:26:25 +0000

    https://sdmntpreastus.oaiusercontent.com/files/00000000-ff24-61f9-aa93-7

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'sdmntpreastus.oaiusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -170,7 +170,7 @@ export default function Home() {
           </p>
           <div className="flex justify-center my-8">
             <Image
-              src="https://firebasestorage.googleapis.com/v0/b/firebase-studio-demos.appspot.com/o/projects%2Fproj_flH2ww%2F51821a4f-a9b0-4a8f-b98a-4402a7b82f7c.jpeg?alt=media&token=e93a61b8-2e06-4407-a068-07e0b5d922c2"
+              src="https://sdmntpreastus.oaiusercontent.com/files/00000000-ff24-61f9-aa93-7a7b1c83cb26/raw?se=2025-08-31T16%3A25%3A08Z&sp=r&sv=2024-08-04&sr=b&scid=fe6c2531-46ad-5a33-991d-b36c1b8393fc&skoid=c156db82-7a33-468f-9cdd-06af263ceec8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-08-30T19%3A16%3A31Z&ske=2025-08-31T19%3A16%3A31Z&sks=b&skv=2024-08-04&sig=/fmlqa5c3Z46PaPDVly5u3t3m/yQ9sGxVeuPRw2zEc8%3D"
               alt="Pack 200 Áudios Livros Best-Sellers"
               width={600}
               height={600}
