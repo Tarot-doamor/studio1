53458d31fabb3d81e0b4ad5943be53902f7d91d1
Thiago alves <gt584138@gmail.com>
2025-09-07 00:54:14 +0000

    preciso que exlua todo esse site vamos criar outro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,12 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export const metadata: Metadata = {
-  title: 'GrowthBoost - Seu Próximo Nível',
-  description: '200 Áudios Livros Best-Sellers que transformam sua mentalidade, aceleram seu crescimento pessoal e impulsionam suas finanças.',
+  title: 'Meu Novo App',
+  description: 'Criado com Firebase Studio',
 };
 
 export default function RootLayout({
@@ -18,34 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className="dark">
-      <head>
-        <Script
-          id="fb-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '800264619015626');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=800264619015626&ev=PageView&noscript=1"
-          />
-        </noscript>
-      </head>
       <body className={`${inter.variable} font-sans antialiased`}>
         {children}
         <Toaster />
