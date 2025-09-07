14170459eea1e9bbc3f5bd2e4a4babc71e863355
Thiago alves <gt584138@gmail.com>
2025-09-07 02:59:55 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -71,7 +71,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https,
+        protocol: 'https',
         hostname: 'i.postimg.cc',
         port: '',
         pathname: '/**',
